91d3033c1f9e3fc03297df2a0d0852f04532b56e
sai-prasanna <sai.r.prasanna@gmail.com>
2019-10-22 00:26:33 +0530

    Update scorer to use model and tokenizers directly

--- lmproof/scorer.py
+++ lmproof/scorer.py
@@ -2,7 +2,7 @@ from typing import Optional, List
 
 import torch
 from torch.nn import CrossEntropyLoss
-from transformers import AutoTokenizer, AutoModelWithLMHead
+from transformers import AutoTokenizer, AutoModelWithLMHead, PreTrainedTokenizer, PreTrainedModel
 
 class SentenceScorer:
     def score(self, sentences: List[str]) -> List[float]:
@@ -10,31 +10,39 @@ class SentenceScorer:
 
 
 class TransformerLMScorer(SentenceScorer):
-    def __init__(self, tokenizer: str, lm: str, device: str = "cpu", batch_size: int = 1, normalize: bool = False):
+    def __init__(self, tokenizer: PreTrainedTokenizer, model: PreTrainedModel,
+                 device: str = "cpu", batch_size: int = 1,
+                 normalize: bool = False):
         # Load pre-trained model tokenizer (vocabulary)
-        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer)
-        self.model = AutoModelWithLMHead.from_pretrained(lm).to(torch.device(device))
+        self.tokenizer = tokenizer
+        self.device = torch.device(device)
+        self.model = model.to(self.device)
         self.model.eval()
         self.batch_size = batch_size
-        self.device = torch.device(device)
-        self.model.to(self.device)
         self.normalize = False
 
     @classmethod
     def load(cls, language: str, device: str = "cpu") -> 'TransformerLMScorer':
         if language == 'en':
+            # Setting an aribitary batch_size
+            # Ideally we should find the max number of tokens that will compute
+            # And dynamically batch.
             batch_size = 1 if device == "cpu" else 32
-            return cls('gpt2', 'gpt2', device, batch_size)
+            tokenizer = AutoTokenizer.from_pretrained('gpt2')
+            model = AutoModelWithLMHead.from_pretrained('gpt2')
+            return cls(tokenizer, model, device, batch_size)
         else:
-            raise RuntimeError('Unknown language')
+            raise RuntimeError('Language {language} is not supported.')
 
     def score(self, sentences: List[str]) -> List[float]:
         scores = []
         for start_idx in range(0, len(sentences), self.batch_size):
             batch = sentences[start_idx:start_idx + self.batch_size]
-            tokens_batch = [torch.LongTensor(self.tokenizer.encode(s)) for s in batch]
-            # Pad inputs by a valid embedding id (0), anyway we will mask it during loss calculation
-            # and future is masked in casual language models, so will not affect attention.
+            tokens_batch = [torch.LongTensor(self.tokenizer.encode(s))
+                            for s in batch]
+            # Pad inputs by a valid embedding id (0),
+            # we will mask it during loss calculation and future is masked
+            # in casual language models, so will not affect attention.
             inputs_batch = torch.nn.utils.rnn.pad_sequence(
                     tokens_batch, batch_first=True, padding_value=0)
             # Pad by -1 for labels so that CrossEntropyLoss will ignore the ids.
