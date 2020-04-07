efd1a3de5c845d5d87e9f859d653968750dfd308
sai-prasanna <sai.r.prasanna@gmail.com>
2020-04-07 20:41:53 +0530

    Update scorer batching logic
    1. Add attention masks.
    2. Process complete batches with items within
    max_limit.

--- lmproof/scorer.py
+++ lmproof/scorer.py
@@ -24,9 +24,10 @@ class TransformerLMScorer(SentenceScorer):
         tokenizer: PreTrainedTokenizer,
         model: PreTrainedModel,
         device: str = "cpu",
-        batch_size: int = 1,
+        batch_size: int = 32,
         add_special_tokens: bool = False,
         normalize: bool = False,
+        max_token_limit: int = 128,
     ):
         # Load pre-trained model tokenizer (vocabulary)
         self.tokenizer = tokenizer
@@ -35,61 +36,72 @@ class TransformerLMScorer(SentenceScorer):
         self.batch_size = batch_size
         self.normalize = normalize
         self._add_special_tokens = add_special_tokens
-        self._loss_fn = CrossEntropyLoss(ignore_index=-1)
+        self._loss_ignore_idx = -100
+        self._loss_fn = CrossEntropyLoss(ignore_index=self._loss_ignore_idx)
+        self.max_token_limit = max_token_limit
 
     @classmethod
     def load(cls, language: str, device: str = "cpu") -> "TransformerLMScorer":
         if language == "en":
-            # Setting an aribitary batch_size
-            # Ideally we should find the max number of tokens that will compute
-            # And dynamically batch.
-            batch_size = 1 if device == "cpu" else 32
             tokenizer = AutoTokenizer.from_pretrained("gpt2")
             model = AutoModelWithLMHead.from_pretrained("distilgpt2")
-            return cls(tokenizer, model, device, batch_size)
+            return cls(tokenizer, model, device)
         else:
             raise RuntimeError(f"Language {language} is not supported.")
 
+    def _predict(
+        self, input_ids: List[torch.LongTensor], attention_mask: List[torch.LongTensor]
+    ) -> List[float]:
+        padded_batch = torch.nn.utils.rnn.pad_sequence(
+            input_ids, batch_first=True, padding_value=0
+        ).to(self.device)
+        padded_mask = torch.nn.utils.rnn.pad_sequence(
+            attention_mask, batch_first=True, padding_value=0
+        ).to(self.device)
+        labels_batch = torch.nn.utils.rnn.pad_sequence(
+            input_ids, batch_first=True, padding_value=self._loss_ignore_idx
+        ).to(self.device)
+        with torch.no_grad():
+            lm_logits = self.model(input_ids=padded_batch, attention_mask=padded_mask)[
+                0
+            ]
+            shift_logits_batch = lm_logits[..., :-1, :].contiguous()
+            shift_labels_batch = labels_batch[..., 1:].contiguous()
+            scores = []
+            for shift_logits, shift_labels in zip(
+                shift_logits_batch, shift_labels_batch
+            ):
+                loss = self._loss_fn(shift_logits, shift_labels)
+                score = -loss
+                if self.normalize:
+                    score = score / (padded_mask.sum() - 1)
+                scores.append(score.cpu().item())
+        return scores
+
     def score(self, sentences: List[str]) -> List[Optional[float]]:
-        scores: List[Optional[float]] = []
-        for start_idx in range(0, len(sentences), self.batch_size):
-            batched_sentences = sentences[start_idx : start_idx + self.batch_size]
+        encoded = self.tokenizer.batch_encode_plus(sentences)
+        scores: List[Optional[float]] = [None] * len(sentences)
+        batch_input_ids = []
+        batch_attention_mask = []
+        batch_idx = []
+        batch_size = 2
+        for i in range(len(sentences)):
+            if len(encoded["input_ids"][i]) < self.max_token_limit:
+                batch_idx.append(i)
+                input_ids = torch.LongTensor(encoded["input_ids"][i])  # type: ignore
+                attention_mask = torch.LongTensor(encoded["attention_mask"][i])  # type: ignore
+                batch_input_ids.append(input_ids)
+                batch_attention_mask.append(attention_mask)
 
-            batch_scores: List[Optional[float]] = [None] * len(batched_sentences)
-            batch_scored_idx = []
-
-            tokenized_batch = []
-            for i, sentence in enumerate(batched_sentences):
-                tokens = self.tokenizer.encode(
-                    sentence, add_special_tokens=self._add_special_tokens
-                )
-                if len(tokens) <= self.tokenizer.max_len:
-                    tokenized_batch.append(torch.LongTensor(tokens))  # type: ignore
-                    batch_scored_idx.append(i)
-            if tokenized_batch:
-                # Pad inputs by a valid embedding id (0),
-                # we will mask it during loss calculation and future is masked
-                # in casual language models, so will not affect attention.
-                inputs_batch = torch.nn.utils.rnn.pad_sequence(
-                    tokenized_batch, batch_first=True, padding_value=0
-                )
-                # Pad by -1 for labels so that CrossEntropyLoss will ignore the ids.
-                labels_batch = torch.nn.utils.rnn.pad_sequence(
-                    tokenized_batch, batch_first=True, padding_value=-1
-                )
-                inputs_batch = inputs_batch.to(self.device)
-                labels_batch = labels_batch.to(self.device)
-
-                lm_logits = self.model(inputs_batch)[0]
-                shift_logits = lm_logits[..., :-1, :].contiguous()
-                shift_labels = labels_batch[..., 1:].contiguous()
-
-                for i in range(shift_logits.size(0)):
-                    loss = self._loss_fn(shift_logits[i], shift_labels[i])
-                    score = -loss.item()
-                    if self.normalize:
-                        score = score / (tokenized_batch[i].size(0) - 1)
-                    batch_scores[batch_scored_idx[i]] = score
-            scores.extend(batch_scores)
-
+            if len(batch_input_ids) == self.batch_size:
+                batch_scores = self._predict(batch_input_ids, batch_attention_mask)
+                for idx, score in zip(batch_idx, batch_scores):
+                    scores[idx] = score
+                batch_input_ids = []
+                batch_attention_mask = []
+                batch_idx = []
+        if len(batch_input_ids) > 0:
+            batch_scores = self._predict(batch_input_ids, batch_attention_mask)
+            for idx, score in zip(batch_idx, batch_scores):
+                scores[idx] = score
         return scores
