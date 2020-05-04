4fd560b1cb577f54b6e1fe930bcb2039b352cdef
gurunath.p <gurunath.p@zohocorp.com>
2020-05-04 16:53:53 +0530

    Take in replacement pairs for MatchedGenerator in the form of dictionary of words and their corresponding replacements

--- lmproof/candidate_generators.py
+++ lmproof/candidate_generators.py
@@ -1,5 +1,5 @@
 import pathlib
-from typing import List, Set, Tuple
+from typing import List, Set, Dict
 
 import lemminflect
 import spacy
@@ -17,21 +17,10 @@ class CandidateEditGenerator:
 
 class MatchedGenerator(CandidateEditGenerator):
     def __init__(
-        self,
-        substitutions: List[Tuple[str, ...]],
-        spacy_model: spacy.language.Language,
-        generate_combinations: bool = True,
+        self, substitutions: Dict[str, Set[str]], spacy_model: spacy.language.Language,
     ):
         self._spacy = spacy_model
-        if generate_combinations:
-            self._word2substitutes = {
-                word: set(substs) for substs in substitutions for word in substs if word
-            }
-            print(f"w2s type: {type(self._word2substitutes)}")
-        else:  # Create one-to-one substitute mapping unidirectionally
-            self._word2substitutes = {
-                substitution[0]: {substitution[1]} for substitution in substitutions
-            }
+        self._word2substitutes = substitutions
 
     @classmethod
     def load(cls, language: str) -> "MatchedGenerator":
--- lmproof/substitutions.py
+++ lmproof/substitutions.py
@@ -1,14 +1,19 @@
 # pylint:disable=bad-continuation
-from typing import List, Tuple
+from typing import List, Set, Dict, Optional
 
 
-def english() -> List[Tuple[str, ...]]:
-    return [
+def english() -> Dict[str, Set[str]]:
+    substitutions = [
         # Article/Determiner
-        ("a", "an", "the", ""),
+        ["a", "an", "the", ""],
         # Prepositions
-        ("about", "at", "by", "for", "from", "in", "of", "on", "to", "with", ""),
+        ["about", "at", "by", "for", "from", "in", "of", "on", "to", "with", ""],
         # Commonly confused words
-        ("bear", "bare"),
-        ("lose", "loose"),
+        ["bear", "bare"],
+        ["lose", "loose"],
     ]
+    return {
+        each_element: set(replacement_list) - {each_element}
+        for replacement_list in substitutions
+        for each_element in replacement_list
+    }
